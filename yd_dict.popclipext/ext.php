6ae953425033d75d4e83200966c3cbd7c5ee42c3
wweir <wweir@foxmail.com>
2018-11-09 00:28:02 +0800

    Dict:  refactor youdao dict

--- yd_dict.popclipext/ext.php
+++ yd_dict.popclipext/ext.php
@@ -1,5 +1,6 @@
 #!/usr/bin/env php
 <?php
+
 $ch = curl_init();
 curl_setopt_array($ch, [
     CURLOPT_URL => 'https://aidemo.youdao.com/trans',
@@ -9,7 +10,4 @@ curl_setopt_array($ch, [
 $resp = curl_exec($ch);
 curl_close($ch);
 
-foreach (json_decode($resp, true)['translation'] as $key => $value) {
-    $ret .= $value;
-}
-echo $ret;
+echo @join('; ', @(json_decode($resp, true))['translation']);
