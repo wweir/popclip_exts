e594277a114100433486a08e99afedda0721d81a
wweir <wweir@foxmail.com>
2018-11-09 12:02:51 +0800

    Add word snake/camel case convertor

---
+++ case_convert.popclipext/ext.php
@@ -0,0 +1,18 @@
+#!/usr/bin/env php
+<?php
+$word = getenv('POPCLIP_TEXT');
+if (strpos($word, '_')) {
+    exit(toCamelCase($word));
+}
+echo toSnakeCase($word);
+
+function toCamelCase($uncamelized_words, $separator = '_')
+{
+    $uncamelized_words = str_replace($separator, " ", strtolower($uncamelized_words));
+    return str_replace(" ", "", ucwords($uncamelized_words));
+}
+
+function toSnakeCase($camelCaps, $separator = '_')
+{
+    return strtolower(preg_replace('/[A-Z]([a-z]+|[A-Z]+?)(?=[A-Z]|$)/', $separator . "$0", lcfirst($camelCaps)));
+}
